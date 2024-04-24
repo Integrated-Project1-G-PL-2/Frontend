b2c4c88e43bd02e8f7eeefa637d1f1d12bc11509
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-04-24 16:46:34 +0700

    fix tailwind

--- integrated-project-pl2-frontend/src/App.vue
+++ integrated-project-pl2-frontend/src/App.vue
@@ -1,7 +1,18 @@
 <script setup></script>
 
 <template>
-  <div>Fronted!!</div>
+  <div class="w-full p-3 bg-slate-50 flex flex-col gap-2">
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Title</th>
+          <th>Assignees</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
 </template>
 
 <style scoped></style>
