99e21bec32def479389c01e59725d416fa1fdab4
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-04-24 17:04:38 +0700

    fix

--- integrated-project-pl2-frontend/src/App.vue
+++ integrated-project-pl2-frontend/src/App.vue
@@ -1,16 +1,17 @@
 <script setup></script>
 
 <template>
-  <div
-    class="w-full p-3 bg-slate-50 flex flex-col gap-2 border-gray-300 rounded-lg"
-  >
-    <table>
-      <tr>
-        <th></th>
-        <th>Title</th>
-        <th>Assignees</th>
-        <th>Status</th>
-      </tr>
+  <div class="p-5 h-screen bg-gray-100">
+    <h1 class="text-xl mb-2 text-center">IT-Bangmod Kradan Kanban</h1>
+    <table class="w-full">
+      <thead class="bg-gray-50 border-b-2 border-gray-200">
+        <tr>
+          <th>No.</th>
+          <th>Title</th>
+          <th>Assignees</th>
+          <th>Status</th>
+        </tr>
+      </thead>
     </table>
   </div>
 </template>
