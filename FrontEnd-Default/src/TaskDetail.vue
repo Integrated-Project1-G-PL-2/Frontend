ae16c64fae822f9ec47df0a32fa3f050d8f4bd99
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-04-26 22:37:33 +0700

    fix

--- FrontEnd-Default/src/TaskDetail.vue
+++ FrontEnd-Default/src/TaskDetail.vue
@@ -1,8 +1,4 @@
 <script setup>
-import { ref } from 'vue'
-
-import ButtonSetting from './components/ButtonSetting.vue'
-
 defineProps({ id: String })
 </script>
 
@@ -169,11 +165,11 @@ Pang;Pong</textarea
     class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[100px]"
   >
     <div class="w-[90%] m-[auto]">
-      <div
-        class="flex flex-col justify-between bg-white p-4 border-gray-200 rounded-md"
-      >
+      <div class="flex flex-col justify-between bg-white p-4">
         <div class="w-full h-[10%] mt-2">
-          <h1 class="text-xl font-bold">Design Backend API for PBI 1,2</h1>
+          <h1 class="itbkk-title text-xl font-bold text-justify">
+            Design Backend API for PBI 1,2
+          </h1>
         </div>
         <div class="border-b w-full mt-4"></div>
         <div class="flex flex-row">
@@ -181,7 +177,7 @@ Pang;Pong</textarea
             <div class="pl-4 mt-4">Description</div>
             <div class="w-full h-[420px]">
               <textarea
-                class="w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bg-gray-100 text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                class="w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                 placeholder="Enter your text here..."
               ></textarea>
             </div>
@@ -191,7 +187,7 @@ Pang;Pong</textarea
               <div class="pl-4 mt-4">Assignees</div>
               <div class="h-[150px]">
                 <textarea
-                  class="w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bg-gray-100 text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                  class="w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bbg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                   placeholder="Enter your text here..."
                 ></textarea>
               </div>
@@ -201,7 +197,9 @@ Pang;Pong</textarea
                 <div class="label">
                   <span class="label-text">Status</span>
                 </div>
-                <select class="select select-bordered w-full bg-inherit">
+                <select
+                  class="itbkk-status select select-bordered w-full bg-inherit border-gray-100 rounded-md text-gray-300"
+                >
                   <option disabled selected>Status</option>
                   <option>To do</option>
                   <option>Doing</option>
@@ -210,26 +208,32 @@ Pang;Pong</textarea
               </label>
             </div>
             <div class="mt-10 ml-4">
-              <div class="">
-                <div>TimeZone</div>
+              <div class="itbkk-timezone">
+                <div>TimeZone :</div>
                 <div></div>
               </div>
-              <div class="">
-                <div>Created On</div>
+              <div class="itbkk-created-on">
+                <div>Created On :</div>
                 <div></div>
               </div>
-              <div class="">
-                <div>Updated On</div>
+              <div class="itbkk-updated-on">
+                <div>Updated On :</div>
                 <div></div>
               </div>
             </div>
           </div>
         </div>
-        <div class="flex flex-row w-full justify-end">
-          <div class="mr-2">
+        <div
+          class="flex flex-row w-full justify-end itbkk-button bg-gray-50 relative border h-[48px] gap-2"
+        >
+          <div
+            class="itbkk-button bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[10px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200"
+          >
             <div @click="$emit('setDetail', false)" class="btn">Ok</div>
           </div>
-          <div class="">
+          <div
+            class="itbkk-button bg-gray-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[10px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200"
+          >
             <div @click="$emit('setDetail', false)" class="btn">close</div>
           </div>
         </div>
