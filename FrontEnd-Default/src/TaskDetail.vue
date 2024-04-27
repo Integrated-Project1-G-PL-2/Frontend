06e1911ff1a48247426624b935dae55b87e69110
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-04-28 01:08:17 +0700

    ดรป

--- FrontEnd-Default/src/TaskDetail.vue
+++ FrontEnd-Default/src/TaskDetail.vue
@@ -1,12 +1,10 @@
-<script setup>
-defineEmits(['closePopUp'])
-</script>
+<script setup></script>
 
 <template>
   <div
-    class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[100px] flex-wrap"
+    class="bg-grey-500 backdrop-blur-sm w-screen text-wrap fixed top-0 left-0 pt-[100px] h-screen"
   >
-    <div class="w-full m-[auto] sm:w-[90%] max-w-[960px]">
+    <div class="w-[90%] m-[auto]">
       <div class="flex flex-col justify-between bg-white p-4">
         <div class="itbkk-title w-full h-[10%] mt-2">
           <h1 class="text-xl font-bold text-justify">
@@ -14,23 +12,22 @@ defineEmits(['closePopUp'])
           </h1>
         </div>
         <div class="border-b w-full mt-4"></div>
-        <div class="flex flex-col sm:flex-row">
-          <div class="w-full sm:w-[70%] h-[50%]">
+        <div class="flex flex-row">
+          <div class="w-[70%] h-[50%]">
             <div class="pl-4 mt-4">Description</div>
-            <div class="w-full h-auto">
+            <div class="w-full h-[420px]">
               <textarea
                 v-model="description"
-                class="itbkk-description w-full sm:w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
+                class="itbkk-description w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
               ></textarea>
             </div>
           </div>
-          <div class="w-full sm:w-[30%] h-[50%] flex-col">
+          <div class="w-[30%] h-[50%] flex-col">
             <div class="h-[180px]">
               <div class="pl-4 mt-4">Assignees</div>
               <div class="h-[150px]">
                 <textarea
-                  v-model="taskAssignees"
-                  class="itbkk-assignees w-full sm:w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bbg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
+                  class="itbkk-assignees w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bbg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
                 ></textarea>
               </div>
             </div>
@@ -40,8 +37,7 @@ defineEmits(['closePopUp'])
                   <span class="label-text ml-4">Status</span>
                 </div>
                 <select
-                  v-model="taskStatus"
-                  class="itbkk-status mt-1 ml-4 select select-bordered w-full sm:w-[95%] h-[40px] px-4 py-2 bg-inherit border-2 border-gray-200 text-gray-400 rounded-md"
+                  class="itbkk-status mt-1 ml-4 select select-bordered w-[95%] h-[40px] px-4 py-2 bg-inherit border-2 border-gray-200 text-gray-400 rounded-md"
                 >
                   <option disabled selected>Status</option>
                   <option>To do</option>
@@ -66,19 +62,18 @@ defineEmits(['closePopUp'])
             </div>
           </div>
         </div>
-        <div
-          class="flex flex-col sm:flex-row w-full justify-end border-t flex-wrap"
-        >
+        <div class="flex flex-row w-full justify-end border-t">
           <button
-            class="itbkk-button bg-green-400 sm:mr-3 mb-2 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-2 flex-wrap"
+            class="itbkk-button bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-wrap text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-2"
           >
             <div class="btn text-center">Ok</div>
           </button>
           <button
-            @click="$emit('closePopUp', false)"
-            class="itbkk-button bg-gray-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-2 flex-wrap"
+            class="itbkk-button bg-gray-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-wrap text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-2"
           >
-            <div class="btn text-center">Close</div>
+            <div class="btn text-center" @click="$emit('closePopUp', false)">
+              Close
+            </div>
           </button>
         </div>
       </div>
