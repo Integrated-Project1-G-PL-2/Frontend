33d401e75342f253ed6660d645d95b9b6c3be685
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-02 02:53:47 +0700

    Update TaskManager.js

--- FrontEnd-Default/src/utils/TaskManager.js
+++ FrontEnd-Default/src/utils/TaskManager.js
@@ -1,5 +1,6 @@
 import { reactive } from 'vue'
 export default class TaskManagement {
+  _taskStorage = []
   constructor() {
     this.tasks = reactive([])
   }
@@ -16,45 +17,12 @@ export default class TaskManagement {
       this.tasks.push(task)
     })
   }
-  _taskDetails = [];
-  constructor(title, assignees, status) {
-    this.title = title;
-    this.assignees = assignees;
-    this.status = status;
-  }
-  findTask(id) {
-    return this._taskDetails.find((detail) => detail.id === id);
-  }
-  findIndexItem(id) {
-    return this._taskDetails.findIndex((detail) => detail.id === id);
-  }
-  toggleUsedItem(id) {
-    this.findTask(id).toggleUsed();
-  }
-  UnusedAllItem() {
-    this._items.forEach((item) => (item.isUsed = false));
-  }
 
-  removeItembyUsedItem() {
-    this.getAllItemUsed().forEach(({ id }) => this.removeItem(id));
-  }
-  removeItem(id) {
-    this.__taskDetails.splice(this.findIndexItem(id), 1);
-  }
-  clearAll() {
-    this.__taskDetails = [];
-  }
-  getAllTask() {
-    return this.__taskDetails;
-  }
   addItem(typeItem) {
-    if (this._taskDetails.length < 0)
-      this._taskDetails.push(new Item(typeItem));
-  }
-  
-  getAllItemUsed() {
-    return this._taskDetails.filter(({ isUsed }) => isUsed);
+    if (this._taskStorage.length < 0) this._taskStorage.push(new Item(typeItem))
   }
 
-  
+  getAllItemUsed() {
+    return this._taskStorage.filter(({ isUsed }) => isUsed)
+  }
 }
