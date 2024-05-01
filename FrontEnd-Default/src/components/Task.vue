9fb105ab96e00a926133cb3b8d1ba1a45f5ba7ea
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-01 21:35:17 +0700

    fix message

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -21,6 +21,7 @@ const path = reactive({})
 const taskBox = ref(new TaskManager())
 const showAddTaskDetail = ref(false)
 const showDeleteTaskDetail = ref(false)
+const showAddAlert = ref(true) // open = true
 const showRedAlert = ref(false) // open = true
 const showGreenAlert = ref(false) // open = true
 
@@ -120,12 +121,41 @@ const saveTaskDetail = async () => {
   <div class="bg-white relative border rounded-lg overflow-auto">
     <h1 class="font-bold text-center">IT-Bangmod Kradan Kanban</h1>
     <div
+      class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+      role="alert"
+      v-show="showAddAlert"
+    >
+      <strong class="font-bold">Success!!</strong>
+      <p>
+        <span class="itbkk-message block sm:inline">
+          The task "" is added successfully</span
+        >
+      </p>
+      <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+        <svg
+          class="fill-current h-6 w-6 text-green-500"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          @click="showAddAlert = false"
+        >
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+      </span>
+    </div>
+    <div
       class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
       role="alert"
       v-show="showRedAlert"
     >
-      <strong class="font-bold">Error!</strong>
-      <span class="block sm:inline"> An error occured deleting the task.</span>
+      <strong class="font-bold">Error!!</strong>
+      <p>
+        <span class="itbkk-message block sm:inline">
+          An error occurred deleting the task""</span
+        >
+      </p>
       <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
         <svg
           class="fill-current h-6 w-6 text-red-500"
@@ -144,8 +174,12 @@ const saveTaskDetail = async () => {
       role="alert"
       v-show="showGreenAlert"
     >
-      <strong class="font-bold">Successful!</strong>
-      <span class="block sm:inline"> Delete completed.</span>
+      <strong class="font-bold">Success!!</strong>
+      <p>
+        <span class="itbkk-message block sm:inline">
+          The task "" has been deleted</span
+        >
+      </p>
       <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
         <svg
           class="fill-current h-6 w-6 text-green-500"
