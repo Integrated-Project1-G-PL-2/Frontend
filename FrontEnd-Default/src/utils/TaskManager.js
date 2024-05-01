8069aad4e04962df39b8f928363458eefce8618f
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-02 02:45:49 +0700

    Update TaskManager.js

--- FrontEnd-Default/src/utils/TaskManager.js
+++ FrontEnd-Default/src/utils/TaskManager.js
@@ -22,8 +22,39 @@ export default class TaskManagement {
     this.assignees = assignees;
     this.status = status;
   }
-  addItem(typeItem) {
-    if (this._items.length < this.limitItem)
-      this._items.push(new Item(typeItem));
+  findTask(id) {
+    return this._taskDetails.find((detail) => detail.id === id);
   }
+  findIndexItem(id) {
+    return this._taskDetails.findIndex((detail) => detail.id === id);
+  }
+  toggleUsedItem(id) {
+    this.findTask(id).toggleUsed();
+  }
+  UnusedAllItem() {
+    this._items.forEach((item) => (item.isUsed = false));
+  }
+
+  removeItembyUsedItem() {
+    this.getAllItemUsed().forEach(({ id }) => this.removeItem(id));
+  }
+  removeItem(id) {
+    this.__taskDetails.splice(this.findIndexItem(id), 1);
+  }
+  clearAll() {
+    this.__taskDetails = [];
+  }
+  getAllTask() {
+    return this.__taskDetails;
+  }
+  addItem(typeItem) {
+    if (this._taskDetails.length < 0)
+      this._taskDetails.push(new Item(typeItem));
+  }
+  
+  getAllItemUsed() {
+    return this._taskDetails.filter(({ isUsed }) => isUsed);
+  }
+
+  
 }
