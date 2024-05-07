64898ec4eee653c25ada79ef31ac0371557a9f77
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-07 14:50:29 +0700

    Update StatusPopUp.vue

--- FrontEnd-Default/src/components/StatusPopUp.vue
+++ FrontEnd-Default/src/components/StatusPopUp.vue
@@ -128,58 +128,7 @@ const handleClick = async () => {
               ></textarea>
             </div>
           </div>
-          <div class="w-[30%] h-[50%] flex-col">
-            <div class="h-[180px]">
-              <div class="pl-4 mt-4">Assignees</div>
-              <div class="h-[150px]">
-                <textarea
-                  :disabled="operate == 'show'"
-                  v-model="task.taskAssignees"
-                  class="itbkk-assignees w-[95%] h-[90%] px-4 py-2 mx-4 my-2 bbg-white text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                  :class="
-                    task.taskAssignees == null ? 'italic text-gray-500 ' : ''
-                  "
-                  placeholder="Unassigned"
-                ></textarea>
-              </div>
-            </div>
-            <div class="w-full h-[100px]">
-              <label class="form-control w-full pl-4">
-                <div class="label">
-                  <span class="label-text ml-4">Status</span>
-                </div>
-                <select
-                  :disabled="operate == 'show'"
-                  v-model="task.taskStatus"
-                  class="itbkk-status mt-1 ml-4 select select-bordered w-[95%] h-[40px] px-4 py-2 bg-inherit border-2 border-gray-200 text-gray-400 rounded-md"
-                >
-                  <option disabled selected>Status</option>
-                  <option value="TO_DO">To Do</option>
-                  <option value="DOING">Doing</option>
-                  <option value="DONE">Done</option>
-                  <option value="NO_STATUS">No Status</option>
-                </select>
-              </label>
-            </div>
-            <div class="mt-10 ml-4">
-              <div class="itbkk-timezone">
-                <div>
-                  TimeZone :
-                  {{ Intl.DateTimeFormat().resolvedOptions().timeZone }}
-                </div>
-                <div></div>
-              </div>
-              <div class="itbkk-created-on">
-                <div>Created On : {{ task.createdOn }}</div>
-                <div></div>
-              </div>
-              <div class="itbkk-updated-on">
-                <div>Updated On : {{ task.updatedOn }}</div>
-                <div></div>
-              </div>
-            </div>
-          </div>
-        </div>
+         
         <div class="flex flex-row w-full justify-end border-t">
           <button
             class="itbkk-button-confirm bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center gap-2 hover:text-gray-200 mr-3 mt-2"
