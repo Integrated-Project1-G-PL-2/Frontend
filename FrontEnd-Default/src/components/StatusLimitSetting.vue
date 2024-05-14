096884230328efbfb15a61b634cecdbea33ef465
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-15 01:30:55 +0700

    fix

---
+++ FrontEnd-Default/src/components/StatusLimitSetting.vue
@@ -0,0 +1,60 @@
+<script setup></script>
+
+<template>
+  <div
+    class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[10px]"
+  >
+    <div
+      class="max-w-[500px] mx-auto mt-[20%] border border-gray-600 h-[200px]"
+    >
+      <div class="flex flex-col justify-between bg-white p-4 h-[100px]">
+        <div class="itbkk-title w-full h-[70px] mt-1 border-b">
+          <h1 class="text-xl font-bold text-justify">Status Settings</h1>
+        </div>
+
+        <div
+          class="flex flex-row items-center justify-between w-[70%] h-[100%]"
+        >
+          <div class="pl-4 mt-4">
+            Users can limit the number of task in a status by setting the
+            Maximum tasks in each status(except "No Status" and "Done"
+            statuses).
+            <select
+              v-model="statusSelect"
+              class="itbkk-status mt-1 ml-4 select select-bordered w-[150px] h-[30px] px-2 py-1 bg-inherit border-2 border-gray-200 text-gray-400 rounded-md text-sm text-justify"
+            >
+              <option
+                v-for="del in filterStatus()"
+                :key="del.id"
+                :value="del.id"
+              >
+                {{ del.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="flex flex-row w-full justify-end border-t h-[60%] mt-6">
+          <button
+            class="itbkk-button-confirm bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[60px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+          >
+            <div class="btn text-center">Save</div>
+          </button>
+
+          <button
+            class="itbkk-button-cancel bg-red-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+            @click="
+              ;[
+                $emit('cancelStatusDetail', true),
+                $router.replace({ name: 'StatusList' })
+              ]
+            "
+          >
+            <div class="btn text-center">Cancel</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -161,6 +161,26 @@ const showStatusesList = function () {
       :operate="'edit'"
     />
     <div class="flex justify-end">
+      <div class="flex justify-start">
+        <input
+          type="text"
+          class="itbkk-status-filter bg-gray-50 border text-gray-400 text-sm rounded-lg"
+          placeholder="Filter by status(es)"
+          required
+        />
+        <svg
+          class="fill-current h-6 w-6 text-gray-500"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          @click="$emit('closePopUp')"
+        >
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+      </input>
+      </div>
       <button
         @click="showAddPopUpTaskDetail('add')"
         class="itbkk-button-add bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 text-gray-100 hover:text-gray-200 mr-3 mt-2"
