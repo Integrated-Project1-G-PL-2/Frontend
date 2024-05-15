c10b251a4e69aa3880b3fe51472cc7efc66502a1
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-05-16 02:08:58 +0700

    fix

--- FrontEnd-Default/src/stores/SortManager.js
+++ FrontEnd-Default/src/stores/SortManager.js
@@ -5,7 +5,7 @@ function sortByTitle(tasks) {
 }
 function sortByTitleReverse(tasks) {
   tasks.sort((a, b) =>
-    a.title.localeCompare(b.title, 'th', { sensitivity: 'base' })
+    b.title.localeCompare(a.title, 'th', { sensitivity: 'base' })
   )
 }
 
