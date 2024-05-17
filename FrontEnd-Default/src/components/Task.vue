c843e191b443b3331601f94f940d298147199334
NoireChama <111353702+NoireChama@users.noreply.github.com>
2024-05-17 13:56:02 +0700

    ลบได้แต่แถบเอ๋อ

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -241,7 +241,7 @@ watch(searchStatus, (status) => {
           v-model="searchStatus"
           
         >
-        <option v-for="(task,index) in cloneTaskGroups" :key="index" > 
+        <option v-for="(task,index) in cloneTaskGroups" :key="index"> 
           {{ task.status.name }} 
         </option>
         
@@ -259,7 +259,25 @@ watch(searchStatus, (status) => {
           />
         </svg>
       </div>
-     
+      <div class="container p-4 border rounded-lg mr-2 ml-2 my-3 w-[650px]">
+        <div
+          v-for="(statusName,index) in collectStatus"
+          class="flex items-center space-x-2 mb-2 border bo "
+        >
+          {{ statusName }} 
+          <svg
+          class="itbkk-filter-clear fill-current h-6 w-6 text-gray-400 cursor-pointer"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          @click="collectStatus = collectStatus.splice(index,1)"
+        >
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+        </div>
+      </div>
       <button
         @click="showAddPopUpTaskDetail('add')"
         class="itbkk-button-add bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 text-gray-100 hover:text-gray-200 mr-2 my-3"
