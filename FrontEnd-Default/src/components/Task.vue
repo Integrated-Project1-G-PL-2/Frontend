c5c4c60a9702fb774d753a293f9c930e6b71b4be
NoireChama <111353702+NoireChama@users.noreply.github.com>
2024-05-17 14:02:12 +0700

    เพิ่มเติม

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -241,8 +241,10 @@ watch(searchStatus, (status) => {
           v-model="searchStatus"
           
         >
-        <option v-for="(task,index) in cloneTaskGroups" :key="index"> 
+        <option v-for="(task,index) in cloneTaskGroups" :key="index" > 
+         
           {{ task.status.name }} 
+          
         </option>
         
       </select>
@@ -270,7 +272,7 @@ watch(searchStatus, (status) => {
           role="button"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          @click="collectStatus = collectStatus.splice(index,1)"
+          @click="collectStatus = collectStatus.splice(index,1),searchStatus = null"
         >
           <path
             d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
