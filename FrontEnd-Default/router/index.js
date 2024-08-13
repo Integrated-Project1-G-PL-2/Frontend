438ebd5acdde61838352920550afa0d56f1237d8
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-08-13 16:43:32 +0700

    add login page

--- FrontEnd-Default/router/index.js
+++ FrontEnd-Default/router/index.js
@@ -5,11 +5,17 @@ import DeleteTaskDetail from '@/components/DeletePopUp.vue'
 import StatusesList from '@/components/StatusesList.vue'
 import StatusPopUp from '@/components/StatusPopUp.vue'
 import DeleteStatus from '@/components/DeleteStatus.vue'
+import LoginPage from '@/components/LoginPage.vue'
 const history = createWebHistory(import.meta.env.BASE_URL)
 const routes = [
   {
     path: '/',
-    redirect: '/task'
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
   },
   {
     path: '/task',
---
+++ FrontEnd-Default/src/components/LoginPage.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+</script>
+
+<template>
+  <div class="main">
+    <h1>Welcome To ITB-KK</h1>
+    <form action="">
+      <label for="first"> Username: </label>
+      <input
+        type="text"
+        id="first"
+        name="first"
+        placeholder="Enter your Username"
+        required
+      />
+
+      <label for="password"> Password: </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Enter your Password"
+        required
+      />
+
+      <div class="wrap">
+        <button type="signIn" onclick="solve()">Sign in</button>
+      </div>
+    </form>
+  </div>
+</template>
+<style scoped></style>
