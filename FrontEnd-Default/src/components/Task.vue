a73fae2dd3a54bbf177bc2b7c22f8da0a050bfa5
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-08-16 01:42:32 +0700

    Update Task.vue

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -22,6 +22,7 @@ import {
   searchByStatus
 } from '@/stores/SortManager.js'
 import { storeToRefs } from 'pinia'
+import LoginPage from './LoginPage.vue'
 
 const statusManager = useStatusManager()
 const showStatusDetailModal = ref(false)
@@ -36,6 +37,7 @@ const taskManager = useTaskManager()
 const taskDetail = reactive({})
 const showDeleteTaskDetail = ref(false)
 const operation = ref('')
+const returnPage = ref(false)
 const collectStatus = reactive([])
 const greenPopup = reactive({
   add: { state: false, taskTitle: '' },
@@ -166,6 +168,11 @@ watch(collectStatus, async () => {
     )
   )
 })
+
+const returnLoginPage = () => {
+  router.replace({ name: 'Login' })
+  returnPage.value = true
+}
 </script>
 
 <template>
@@ -173,6 +180,35 @@ watch(collectStatus, async () => {
     <h1 class="font-bold text-center cursor-default">
       IT-Bangmod Kradan Kanban
     </h1>
+    <div class="flex flex-col items-end pr-4 font-bold space-y-2">
+      <h1 class="font-bold font-sans cursor-default">User Name</h1>
+      <div class="grid grid-cols-1 items-center justify-items-center space-y-2">
+        <svg
+          class="w-8 h-8 text-gray-800 dark:text-gray-700"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+        <button
+          @click="returnLoginPage"
+          class="bg-blue-500 text-sm rounded-[6px] font-sans text-gray-100 hover:text-gray-600 px-3 py-1"
+        >
+          Log out
+        </button>
+      </div>
+    </div>
+
     <AlertPopUp
       v-if="greenPopup.add.state"
       :titles="
