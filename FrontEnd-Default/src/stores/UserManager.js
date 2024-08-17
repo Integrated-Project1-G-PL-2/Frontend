db9892adea45d08ad52a9d5bd2de9405716ccf63
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-08-17 19:31:30 +0700

    Update UserManager.js

--- FrontEnd-Default/src/stores/UserManager.js
+++ FrontEnd-Default/src/stores/UserManager.js
@@ -9,37 +9,12 @@ export const userManager = defineStore('userManager', () => {
     return users
   }
 
-  const fetchUsers = async function (baseUrl) {
-    try {
-      const response = await axios.get(`${baseUrl}/api/users`)
-      const userData = response.data
-
-      // Clear existing users before adding new data
-      users.splice(0, users.length)
-
-      userData.forEach((user) => {
-        users.push({
-          oid: user.oid,
-          username: user.username,
-          password: user.password,
-          name: user.name,
-          email: user.email,
-          role: user.role
-        })
-      })
-    } catch (error) {
-      console.error('Error fetching users:', error)
-    }
-  }
-
   const compareUser = async function (inputUser) {
     try {
-      // Ensure users are fetched
       if (users.length === 0) {
         await fetchUsers(import.meta.env.VITE_BASE_URL)
       }
 
-      // Compare input with fetched users
       const foundUser = users.find(
         (user) =>
           user.username === inputUser.username &&
@@ -65,7 +40,6 @@ export const userManager = defineStore('userManager', () => {
   }
 })
 
-// For HMR support
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(userManager, import.meta.hot))
 }
