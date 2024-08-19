8f54bb912e413de6fe144b068748d04b5abc5a25
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-08-19 19:13:59 +0700

    Update Task.vue

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -22,7 +22,7 @@ import {
   searchByStatus
 } from '@/stores/SortManager.js'
 import { storeToRefs } from 'pinia'
-import LoginPage from './LoginPage.vue'
+import { userName } from './LoginPage.vue'
 
 const statusManager = useStatusManager()
 const showStatusDetailModal = ref(false)
@@ -181,7 +181,7 @@ const returnLoginPage = () => {
       IT-Bangmod Kradan Kanban
     </h1>
     <div class="flex flex-col items-end pr-4 font-bold space-y-2">
-      <h1 class="font-bold font-sans cursor-default">User Name</h1>
+      <h1 class="font-bold font-sans cursor-default">{{ userName }}</h1>
       <div class="grid grid-cols-1 items-center justify-items-center space-y-2">
         <svg
           class="w-8 h-8 text-gray-800 dark:text-gray-700"
