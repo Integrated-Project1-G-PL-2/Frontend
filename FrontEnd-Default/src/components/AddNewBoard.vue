fc311e36897a75a463fea4b28a643f69b972d041
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-08-28 22:51:24 +0700

    fix

---
+++ FrontEnd-Default/src/components/AddNewBoard.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const deClareemit = defineEmits(['saveDetail', 'cancelDetail'])
+const props = defineProps(['taskId'])
+const router = useRouter()
+const deleteTask = async (deleteId) => {
+  deletedTask.value = await deleteItemById(
+    `${import.meta.env.VITE_BASE_URL}/tasks`,
+    deleteId
+  )
+  if (deletedTask.value == '404') {
+    deClareemit('redAlert')
+    deClareemit('cancelDetail', true)
+    return
+  }
+  taskManager.deleteTask(deleteId)
+  deClareemit('confirmDetail', true)
+}
+</script>
+
+<template>
+  <div
+    class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[10px]"
+  >
+    <div class="w-[59%] m-[auto] border border-green-700 mt-[20%]">
+      <div class="flex flex-col justify-between bg-white p-4 h-[10%]">
+        <div class="itbkk-title w-full h-[40px] mt-1 border-b">
+          <h1 class="text-xl font-bold text-justify">Delete a Task</h1>
+        </div>
+
+        <div class="w-[70%] h-[100%]">
+          <div class="flex pl-4 mt-5">
+            Do you want to delete the task "{{ props.taskId.value.index }}.
+            {{ props.taskId.value.taskTitle }}" ?
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row w-full justify-end border-t h-[60%]">
+        <button
+          class="itbkk-button-confirm bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[60px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4 mb-2"
+          @click="deleteTask(props.taskId.value.id)"
+        >
+          <div class="btn text-center">Confirm</div>
+        </button>
+        <button
+          class="itbkk-button-cancel bg-red-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4 mb-2"
+          @click="
+            ;[$emit('cancelDetail', true), $router.replace({ name: 'Task' })]
+          "
+        >
+          <div class="btn text-center">Cancel</div>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- FrontEnd-Default/src/components/BoardList.vue
+++ FrontEnd-Default/src/components/BoardList.vue
@@ -1,8 +1,19 @@
-<script></script>
+<script>
+import AddNewBoard from "./../components/AddNewBoard.vue"
+const showAddNewBoard = true = ref(true)
+const showAddNewBoardPopUp = function () {
+  showAddNewBoard.value = true
+}
+</script>
 <template>
   <button
     class="bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-1"
   >
     Create personal board
   </button>
+
+  <teleport to="body" v-if="showAddNewBoardPopUp">
+    <AddNewBoard @cancelDetail="clearDeletePopUp" @saveDetail="showDelComplete">
+    </AddNewBoard>
+  </teleport>
 </template>
