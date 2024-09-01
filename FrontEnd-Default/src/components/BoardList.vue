bc9f7b55b7c1889454816b2e22bc698cee8389b1
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-09-01 17:25:46 +0700

    Update BoardList.vue

--- FrontEnd-Default/src/components/BoardList.vue
+++ FrontEnd-Default/src/components/BoardList.vue
@@ -31,51 +31,52 @@ const returnLoginPage = () => {
 
 <template>
   <div
-    class="flex flex-col items-end pr-4 font-bold space-y-2 border-b border-r-slate-500"
+    class="flex flex-col items-end pr-4 font-bold space-y-2 border-b py-2 border-r-slate-500"
   >
-    <!-- Align this button to the left -->
-    <div class="flex justify-start items-start w-full">
+    <div class="flex justify-between items-start w-full">
       <button
         @click="goBackToHomePage"
-        class="itbkk-button-home scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 hover:text-blue-500 mr-3 mt-2 text-blue-400 my-3"
+        class="itbkk-button-home scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 hover:text-blue-500 mr-3 ml-2 mt-2 text-blue-400 my-3"
       >
         🏠 ITB-KK
       </button>
-    </div>
-    <h1 class="itbkk-fullname font-bold font-sans cursor-default">
-      {{ userName }}
-    </h1>
-    <div class="grid grid-cols-1 items-center justify-items-center space-y-2">
-      <svg
-        class="w-8 h-8 text-gray-800 dark:text-gray-700"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-        />
-      </svg>
-      <button
-        @click="returnLoginPage"
-        class="bg-blue-500 text-sm rounded-[6px] font-sans text-gray-100 hover:text-gray-600 px-7 py-1"
-      >
-        Log out
-      </button>
+      <div class="flex flex-col items-center space-y-1">
+        <h1 class="itbkk-fullname font-bold font-sans cursor-default text-xs">
+          {{ userName }}
+        </h1>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-gray-700"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+        <button
+          @click="returnLoginPage"
+          class="bg-blue-500 text-xs rounded-[4px] font-sans text-gray-100 hover:text-gray-600 px-4 py-1 mt-1"
+        >
+          Log out
+        </button>
+      </div>
     </div>
   </div>
   <div class="bg-white relative border rounded-lg overflow-auto">
-    <h1 class="font-bold text-center cursor-default">Board List</h1>
+    <h1 class="font-bold text-center cursor-default text-3xl py-3">
+      Board List
+    </h1>
     <div class="flex flex-col items-end pr-4 font-bold space-y-2">
       <button
-        class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-1"
+        class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-2 mr-2 my-3"
         @click="showAddNewBoardPopUp"
       >
         Create personal board
