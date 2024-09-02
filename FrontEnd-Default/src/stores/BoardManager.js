8c411ae053d4fcdaea1dde8b558a5e1f333999e2
NoireChama <111353702+NoireChama@users.noreply.github.com>
2024-09-02 16:53:13 +0700

    loop board ver.Not fix UI

---
+++ FrontEnd-Default/src/stores/BoardManager.js
@@ -0,0 +1,47 @@
+import { reactive } from 'vue'
+import { defineStore, acceptHMRUpdate } from 'pinia'
+
+export const useBoardManager = defineStore('boardManager', () => {
+    const boards = reactive([])
+    const getBoards = function () {
+      return boards
+    }
+    const setBoards = function (boardsList = []) {
+        boards.length = 0
+      if (boardsList != null) {
+        boardsList.forEach((board) => {
+            boards.push(board)
+        })
+      }
+      console.log(boards)
+    }
+    const addBoard = function (newBoard) {
+      boards.push(newBoard)
+    }
+    const findIndexById = function (id) {
+      return boards.findIndex((el) => el.id === id)
+    }
+    // const editTask = function (id, newTask) {
+    //   const index = findIndexById(id)
+    //   tasks[index] = newTask
+    // }
+  
+    // const deleteTask = function (id) {
+    //   const index = tasks.findIndex((el) => {
+    //     return el.id == id
+    //   })
+    //   tasks.splice(index, 1)
+    // }
+  
+    
+    return {
+      getBoards,
+      setBoards,
+      addBoard,
+      findIndexById
+    }
+  })
+  
+  if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useBoardManager, import.meta.hot))
+  }
