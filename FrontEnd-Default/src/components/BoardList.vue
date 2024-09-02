5f4d35a61472105c7a15236758bbd7f31de0f39b
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-09-03 01:27:28 +0700

    Update BoardList.vue

--- FrontEnd-Default/src/components/BoardList.vue
+++ FrontEnd-Default/src/components/BoardList.vue
@@ -1,13 +1,32 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted,ref } from 'vue'
 import AddNewBoard from './../components/AddNewBoard.vue'
 import { userName } from '@/stores/UserManager'
 import { logout } from '@/stores/UserManager'
 import { useRoute, useRouter } from 'vue-router'
+import { useBoardManager } from '@/stores/BoardManager'
 import AlertPopUp from './../components/AlertPopUp.vue'
+import {
+  getItems,
+  getItemById,
+  deleteItemById,
+  addItem,
+  editItem
+} from '../utils/fetchUtils.js'
+
 const router = useRouter()
 const route = useRoute()
 const error = ref(false)
+
+const boardManager = useBoardManager()
+
+const boardsList = boardManager.getBoards()
+
+onMounted(async () => {
+  boardManager.setBoards(await getItems(`${import.meta.env.VITE_BASE_URL}/v3/boards`))
+})
+console.log(boardsList)
+console.log(boardsList)
 
 const showAddNewBoard = ref(false) // Initial value is false
 const returnPage = ref(false)
@@ -37,6 +56,7 @@ const closeProblemAlter = () => {
 </script>
 
 <template>
+  <!-- <h1>{{ boardsList[0].role }}</h1> -->
   <div
     class="flex flex-col items-end pr-4 font-bold space-y-2 border-b py-2 border-r-slate-500"
   >
@@ -87,7 +107,7 @@ const closeProblemAlter = () => {
       @closePopUp="closeProblemAlter"
       message="Error!!"
       styleType="red"
-    />
+    /> 
     <div class="flex flex-col items-end pr-4 font-bold space-y-2">
       <button
         class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-2 mr-2 my-3"
@@ -103,82 +123,27 @@ const closeProblemAlter = () => {
           <th class="px-4 py-3 cursor-default">No</th>
           <th class="px-4 py-3 cursor-default">Name</th>
           <th class="px-4 py-3 flex items-center space-x-2 cursor-default">
-            <span>action</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="cursor-pointer itbkk-status-sort"
-              width="1.2rem"
-              height="1.2rem"
-              viewBox="0 0 48 48"
-              v-if="!switchDate"
-              @click=";[(switchDefault(), sortByTitle(taskGroups))]"
-            >
-              <path fill="#afacac" d="M38 33V5h-4v28h-6l8 10l8-10z" />
-              <path
-                fill="#afacac"
-                d="M16.8 17.2h-5.3l-1.1 3H6.9L12.6 5h2.9l5.7 15.2H18zm-4.6-2.7H16l-1.9-5.7zm.2 26H20V43H8.4v-1.9L16 30.3H8.4v-2.5h11.4v1.7z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="cursor-pointer itbkk-status-sort"
-              width="1.2rem"
-              height="1.2rem"
-              viewBox="0 0 48 48"
-              v-if="switchSort"
-              @click=";[(switchSortText(), sortByTitleReverse(taskGroups))]"
-            >
-              <path fill="#1e40af" d="M38 33V5h-4v28h-6l8 10l8-10z" />
-              <path
-                fill="#1e40af"
-                d="M16.8 17.2h-5.3l-1.1 3H6.9L12.6 5h2.9l5.7 15.2H18zm-4.6-2.7H16l-1.9-5.7zm.2 26H20V43H8.4v-1.9L16 30.3H8.4v-2.5h11.4v1.7z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="cursor-pointer itbkk-status-sort"
-              width="1.2rem"
-              height="1.2rem"
-              viewBox="0 0 16 16"
-              v-if="switchSort2"
-              @click=";[(switchBack(), sortByTitleDate(taskGroups))]"
-            >
-              <g fill="#1e40af">
-                <path
-                  fill-rule="evenodd"
-                  d="M10.082 5.629L9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"
-                />
-                <path
-                  d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zm-8.46-.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999l.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707z"
-                />
-              </g>
-            </svg>
+            <th class="px-4 py-3 cursor-default">Action</th>
           </th>
         </tr>
       </thead>
       <tbody>
         <tr
-          v-for="(task, index) in taskGroups"
-          :key="task.id"
+          v-for="(board, index) in boardsList"
+          :key="board.id.boardId"
           class="itbkk-item border-b cursor-pointer"
         >
           <td class="px-4 py-3">
             {{ index + 1 }}
             <div
               class="inline-flex"
-              @click="showEditTaskDetail(task.id, 'edit')"
+              
             >
               ⚙️
             </div>
             <div
               class="inline-flex"
-              @click="
-                showDeletePopUpTaskDetail({
-                  id: task.id,
-                  taskTitle: task.title,
-                  index: index + 1
-                })
-              "
+            
             >
               🗑️
             </div>
@@ -186,62 +151,29 @@ const closeProblemAlter = () => {
           <td class="itbkk-title px-4 py-3">
             <div
               class="hover:text-sky-500 cursor-default"
-              @click="showBoardDetail(task.id, 'show')"
+              
             >
-              {{ board.name }}
+            {{ index + 1 }}
             </div>
           </td>
           <td
             class="itbkk-assignees px-4 py-3 cursor-default"
-            :class="task.assignees == null ? 'italic' : ''"
           >
-            {{ task.assignees == null ? 'Unassigned' : task.assignees }}
+            {{ board.board.name }}
           </td>
           <td class="itbkk-status px-4 py-3 cursor-default">
             <div
-              class="w-full bg-emerald-500 flex justify-center rounded-md"
-              :style="{
-                backgroundColor:
-                  task.status.name === 'To Do'
-                    ? '#FFC0CB'
-                    : task.status.name === 'Doing'
-                    ? '#ffff99'
-                    : task.status.name === 'No Status'
-                    ? 'lightgray'
-                    : '#90EE90'
-              }"
+              class="w-full   r rounded-md"
+              
             >
-              <p>{{ task.status.name }}</p>
+              <p>{{ board.role }}</p>
             </div>
           </td>
         </tr>
       </tbody>
     </table>
   </div>
-  <teleport to="body" v-if="showTaskDetailModal">
-    <TaskDetail
-      :taskDetail="taskDetail"
-      @showTaskDetailModal="showTaskDetailModal = false"
-      :operate="operation"
-      @showRedPopup="openRedPopup"
-      @showGreenPopup="openGreenPopup"
-    ></TaskDetail>
-  </teleport>
-  <teleport to="body" v-if="showDeleteTaskDetail">
-    <DeletePopUp
-      @cancelDetail="clearDeletePopUp"
-      @confirmDetail="showDelComplete"
-      @redAlert="openRedPopup"
-      :taskId="taskDetail"
-    >
-    </DeletePopUp>
-  </teleport>
-
-  <Teleport to="body" v-if="showStatusDetailLimit">
-    <StatusLimitSetting
-      @clearLimitPopUp="clearLimitStatusPopUp"
-    ></StatusLimitSetting>
-  </Teleport>
+  
 
   <teleport to="body" v-if="showAddNewBoard">
     <AddNewBoard
