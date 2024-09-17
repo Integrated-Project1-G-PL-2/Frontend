18639d340a83a92e3b81a8467ce9be0892ec8d92
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-09-17 21:17:19 +0700

    Update Task.vue

--- FrontEnd-Default/src/components/Task.vue
+++ FrontEnd-Default/src/components/Task.vue
@@ -216,6 +216,12 @@ const returnLoginPage = () => {
 const goBackToHomeBoard = () => {
   router.replace({ name: 'Board' })
 }
+
+// Reactive variable to track checkbox state
+const isSwitch = ref(false)
+
+// Computed label based on checkbox state
+const toggleLabel = computed(() => (isSwitch.value ? 'Public' : 'Private'))
 </script>
 
 <template>
@@ -361,6 +367,16 @@ const goBackToHomeBoard = () => {
           </svg>
         </div>
       </div>
+      <label class="inline-flex items-center cursor-pointer">
+        <input type="checkbox" v-model="isSwitch" class="sr-only peer" />
+        <div
+          class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+        ></div>
+        <span class="ms-3 text-sm font-medium text-gray-600 mr-3 my-3">
+          {{ toggleLabel }}
+        </span>
+      </label>
+
       <button
         @click="showAddPopUpTaskDetail('add')"
         class="itbkk-button-add bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 text-gray-100 hover:text-gray-200 mr-2 my-3"
