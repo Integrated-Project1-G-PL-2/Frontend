ca87f2b362fdc46354f504a98db6fecc92a2a164
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-09-22 14:23:58 +0700

    Update fetchUtils.js

--- FrontEnd-Default/src/utils/fetchUtils.js
+++ FrontEnd-Default/src/utils/fetchUtils.js
@@ -1,127 +1,117 @@
-import {refreshToken} from '@/stores/UserManager'
+import { refreshToken } from '@/stores/UserManager'
+
+async function fetchWithAuth(url, options, router) {
+  const token = localStorage.getItem('jwt')
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const res = await fetch(url, options)
+
+  if (!res.ok) {
+    if (res.status === 401) {
+      console.log('Access token expired, attempting to refresh...')
+      const newToken = await refreshToken(router)
+      if (newToken) {
+        // Retry the request with the new token
+        options.headers.Authorization = `Bearer ${newToken}`
+        const retryRes = await fetch(url, options)
+        if (retryRes.ok) {
+          return retryRes
+        } else {
+          console.error(`Retry failed with status: ${retryRes.status}`)
+          return retryRes
+        }
+      } else {
+        console.error('Token refresh failed, logging out.')
+        logout()
+        router.replace({ name: 'Login' })
+      }
+    } else {
+      console.error(`Failed to fetch: ${res.status}`)
+    }
+  }
+
+  return res
+}
+
 export async function getItems(url, router) {
   try {
-    const token = localStorage.getItem('jwt');
     const options = {
       method: 'GET',
       headers: {}
-    };
-    
-    if (token) {
-      options.headers.Authorization = `Bearer ${token}`;
     }
 
-    const res = await fetch(url, options);
-
-    if (!res.ok) {
-      if (res.status === 401) {
-        console.log("Access token expired, attempting to refresh...");
-        const newToken = await refreshToken(router);
-        if (newToken) {
-          // Retry the request with the new token
-
-          options.headers.Authorization = `Bearer ${newToken}`;
-          const retryRes = await fetch(url, options);
-          
-          if (retryRes.ok) {
-            return await retryRes.json();
-          } else {
-            console.error(`Retry failed with status: ${retryRes.status}`);
-            return retryRes.status;
-          }
-        } else {
-          console.error("Token refresh failed, logging out.");
-          logout();
-          router.replace({ name: 'Login' });
-        }
-      } else {
-        console.error(`Failed to fetch items: ${res.status}`);
-        return res.status;
-      }
+    const res = await fetchWithAuth(url, options, router)
+    if (res) {
+      return await res.json()
     }
-    
-    return await res.json();
-  } catch (error) {
-    console.error(`Network error: ${error}`);
-    return null;
-  }
-}
-
-
-
-async function getItemById(url, id) {
-  try {
-    const token = localStorage.getItem('jwt')
-    const options = {}
-    if (token) {
-      options.headers = {
-        Authorization: `Bearer ${token}`
-      }
-    }
-    const res = await fetch(`${url}/${id}`, options)
-    if (!res.ok) {
-      console.error(`Failed to fetch item with ID ${id}: ${res.status}`)
-      return null
-    }
-    return await res.json()
+    return null
   } catch (error) {
     console.error(`Network error: ${error}`)
     return null
   }
 }
 
-async function deleteItemById(url, id) {
+async function getItemById(url, id, router) {
   try {
-    const token = localStorage.getItem('jwt')
     const options = {
-      method: 'DELETE'
+      method: 'GET',
+      headers: {}
     }
-    if (token) {
-      options.headers = {
-        Authorization: `Bearer ${token}`
-      }
+
+    const res = await fetchWithAuth(`${url}/${id}`, options, router)
+    if (res) {
+      return await res.json()
     }
-    const res = await fetch(`${url}/${id}`, options)
-    if (!res.ok) {
-     
-      console.error(`Failed to delete item with ID ${id}: ${res.status}`)
+    return null
+  } catch (error) {
+    console.error(`Network error: ${error}`)
+    return null
+  }
+}
+
+async function deleteItemById(url, id, router) {
+  try {
+    const options = {
+      method: 'DELETE',
+      headers: {}
+    }
+
+    const res = await fetchWithAuth(`${url}/${id}`, options, router)
+    if (res) {
       return res.status
     }
-    return res.status
+    return null
   } catch (error) {
     console.error(`Network error: ${error}`)
     return null
   }
 }
 
-async function deleteAndTransferItem(url, id, newId) {
+async function deleteAndTransferItem(url, id, newId, router) {
   try {
-    const token = localStorage.getItem('jwt')
     const options = {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {}
     }
-    if (token) {
-      options.headers = {
-        Authorization: `Bearer ${token}`
-      }
-    }
-    const res = await fetch(`${url}/${id}/${newId}`, options)
-    if (!res.ok) {
-      console.error(
-        `Failed to delete and transfer item with ID ${id} to ${newId}: ${res.status}`
-      )
+
+    const res = await fetchWithAuth(`${url}/${id}/${newId}`, options, router)
+    if (res) {
       return res.status
     }
-    return res.status
+    return null
   } catch (error) {
     console.error(`Network error: ${error}`)
     return null
   }
 }
 
-async function addItem(url, newItem) {
+async function addItem(url, newItem, router) {
   try {
-    const token = localStorage.getItem('jwt')
     const options = {
       method: 'POST',
       headers: {
@@ -130,24 +120,19 @@ async function addItem(url, newItem) {
       body: JSON.stringify(newItem)
     }
 
-    if (token) {
-      options.headers.Authorization = `Bearer ${token}`
+    const res = await fetchWithAuth(url, options, router)
+    if (res) {
+      return await res.json()
     }
-    const res = await fetch(url, options)
-    if (!res.ok) {
-      console.error(`Failed to add item: ${res.status}`)
-      return res.status
-    }
-    return await res.json()
+    return null
   } catch (error) {
     console.error(`Network error: ${error}`)
     return null
   }
 }
 
-async function editItem(url, id, editedItem) {
+async function editItem(url, id, editedItem, router) {
   try {
-    const token = localStorage.getItem('jwt')
     const options = {
       method: 'PUT',
       headers: {
@@ -155,27 +140,16 @@ async function editItem(url, id, editedItem) {
       },
       body: JSON.stringify(editedItem)
     }
-    if (token) {
-      options.headers.Authorization = `Bearer ${token}`
+
+    const res = await fetchWithAuth(`${url}/${id}`, options, router)
+    if (res) {
+      return await res.json()
     }
-    const res = await fetch(`${url}/${id}`, options)
-    if (!res.ok) {
-      console.error(`Failed to edit item with ID ${id}: ${res.status}`)
-      return null
-    }
-    return await res.json()
+    return null
   } catch (error) {
     console.error(`Network error: ${error}`)
     return null
   }
 }
 
-export {
- 
-  getItemById,
-  deleteItemById,
-  addItem,
-  editItem,
-  deleteAndTransferItem,
-
-}
+export { getItemById, deleteItemById, addItem, editItem, deleteAndTransferItem }
