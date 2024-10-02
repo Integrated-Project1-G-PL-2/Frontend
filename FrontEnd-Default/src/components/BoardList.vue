d5b7c5dccc007b84fb29ffcce2ac95b8063db0a8
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-10-02 23:50:58 +0700

    fix css

--- FrontEnd-Default/src/components/BoardList.vue
+++ FrontEnd-Default/src/components/BoardList.vue
@@ -74,161 +74,191 @@ const returnLoginPage = () => {
 </script>
 
 <template>
-  <!-- <h1>{{ boardsList[0] }}</h1> -->
-  <div
-    class="flex flex-col items-end pr-4 font-bold space-y-2 border-b py-2 border-r-slate-500"
-  >
-    <div class="flex justify-between items-start w-full">
-      <button
-        class="itbkk-button-home scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 mr-3 ml-2 mt-2 text-blue-400 my-3 cursor-default"
-      >
-        🏠 ITB-KK
-      </button>
-      <!-- Adjust the height and padding as needed -->
-      <h1 class="font-bold text-center cursor-default text-3xl py-3">
-        Board List
-      </h1>
-      <div class="flex flex-col items-center space-y-1">
-        <h1 class="itbkk-fullname font-bold font-sans cursor-default text-xs">
-          {{ userName }}
-        </h1>
-        <svg
-          class="w-6 h-6 text-gray-800 dark:text-gray-700"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+  <div>
+    <div
+      class="flex flex-col items-end pr-4 font-bold space-y-2 border-b py-2 border-r-slate-500"
+    >
+      <div class="flex justify-between items-start w-full">
         <button
-          @click="returnLoginPage"
-          class="bg-blue-500 text-xs rounded-[4px] font-sans text-gray-100 hover:text-gray-600 px-4 py-1 mt-1"
+          class="itbkk-button-home scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] font-sans btn-xs scr-l:btn-m text-center gap-5 mr-3 ml-2 mt-2 text-blue-400 my-3 cursor-default"
         >
-          Log out
+          🏠 ITB-KK
         </button>
+        <h1 class="font-bold text-center cursor-default text-3xl py-3">
+          Board List
+        </h1>
+        <div class="flex flex-col items-center space-y-1">
+          <h1 class="itbkk-fullname font-bold font-sans cursor-default text-xs">
+            {{ userName }}
+          </h1>
+          <svg
+            class="w-6 h-6 text-gray-800 dark:text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+          <button
+            @click="returnLoginPage"
+            class="bg-blue-500 text-xs rounded-[4px] font-sans text-gray-100 hover:text-gray-600 px-4 py-1 mt-1"
+          >
+            Log out
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  <div
-    class="bg-white relative border rounded-lg overflow-auto max-h-[calc(100vh-10rem)] p-4"
-  >
-    <AlertPopUp
-      v-if="error"
-      :titles="'There is a problem. Please try again later.'"
-      @closePopUp="closeProblemAlter"
-      message="Error!!"
-      styleType="red"
-    />
-    <AlertPopUp
-      v-if="errorPrivate"
-      :titles="'Access denied, you do not have permission to view this page.'"
-      @closePopUp="closePrivateAlter"
-      message="Error!!"
-      styleType="red"
-    />
-    <div class="flex flex-col items-end pr-4 font-bold space-y-2">
-      <button
-        class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-2 mr-2 my-3"
-        @click="showAddNewBoardPopUp"
-      >
-        Create personal board
-      </button>
-    </div>
-    <div class="flex justify-center font-bold">
-      <h1 class="font-bold text-center cursor-default text-3xl py-3">
-        Personal Board
-      </h1>
-    </div>
-    <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-    >
-      <!-- Grid layout for dashboard-like appearance -->
+
+    <!-- New wrapper for both sections -->
+    <div class="my-8 space-y-10">
+      <!-- Personal Board Section -->
       <div
-        v-for="(board, index) in boardsList"
-        :key="board.id.boardId"
-        class="itbkk-personal-item bg-white border rounded-lg shadow-md p-4 flex flex-col space-y-2 cursor-pointer hover:bg-gray-100 hover:text-sky-500"
-        @click="
-          ;[
-            emits('NameBoard', board.board.name),
-            router.replace({ name: 'Task', params: { id: board.id.boardId } })
-          ]
-        "
+        class="bg-white relative border rounded-lg overflow-auto max-h-[calc(100vh-10rem)] p-4"
       >
-        <div class="flex justify-between items-center">
-          <span class="font-bold text-lg">No : {{ index + 1 }}</span>
-          <!-- <div class="flex space-x-2">
-            <span>⚙️</span>
-            <span>🗑️</span>
-          </div> -->
+        <AlertPopUp
+          v-if="error"
+          :titles="'There is a problem. Please try again later.'"
+          @closePopUp="closeProblemAlter"
+          message="Error!!"
+          styleType="red"
+        />
+        <AlertPopUp
+          v-if="errorPrivate"
+          :titles="'Access denied, you do not have permission to view this page.'"
+          @closePopUp="closePrivateAlter"
+          message="Error!!"
+          styleType="red"
+        />
+        <div class="flex flex-col items-end pr-4 font-bold space-y-2">
+          <button
+            class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-2 mr-2 my-3"
+            @click="showAddNewBoardPopUp"
+          >
+            Create personal board
+          </button>
         </div>
-        <div class="itbkk-board-name text-xl font-semibold">
-          Name :
-          {{ board.board?.name == undefined ? board.name : board.board.name }}
+        <div class="flex justify-center font-bold">
+          <h1 class="font-bold text-center cursor-default text-3xl py-3">
+            Personal Board
+          </h1>
         </div>
-        <div class="text-sm text-gray-500">
-          <p>Action : {{ board.role == undefined ? 'owner' : board.role }}</p>
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        >
+          <div
+            v-for="(board, index) in boardsList"
+            :key="board.id.boardId"
+            class="itbkk-personal-item bg-white border rounded-lg shadow-md p-4 flex flex-col space-y-2 cursor-pointer hover:bg-gray-100 hover:text-sky-500"
+            @click="
+              ;[
+                emits('NameBoard', board.board.name),
+                router.replace({
+                  name: 'Task',
+                  params: { id: board.id.boardId }
+                })
+              ]
+            "
+          >
+            <div class="flex justify-between items-center">
+              <span class="font-bold text-lg">No : {{ index + 1 }}</span>
+            </div>
+            <div class="itbkk-board-name text-xl font-semibold">
+              Name :
+              {{
+                board.board?.name == undefined ? board.name : board.board.name
+              }}
+            </div>
+            <div class="text-sm text-gray-500">
+              <p>
+                Action : {{ board.role == undefined ? 'owner' : board.role }}
+              </p>
+            </div>
+            <div class="itbkk-board-visibility text-sm text-gray-500">
+              <p>
+                Visibility :
+                {{ board.role == undefined ? 'owner' : board.role }}
+              </p>
+            </div>
+          </div>
         </div>
-        <div class="itbkk-board-visibility text-sm text-gray-500">
-          <p>
-            Visibility : {{ board.role == undefined ? 'owner' : board.role }}
-          </p>
+      </div>
+
+      <!-- Collab Board Section -->
+      <div
+        class="bg-white relative border rounded-lg overflow-auto max-h-[calc(100vh-10rem)] p-4"
+      >
+        <div class="flex justify-center font-bold">
+          <h1 class="font-bold text-center cursor-default text-3xl py-3">
+            Collab Boards
+          </h1>
+        </div>
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        >
+          <div
+            v-for="(board, index) in boardsList"
+            :key="board.id.boardId"
+            class="itbkk-collab-item bg-white border rounded-lg shadow-md p-4 flex flex-col space-y-2 cursor-pointer hover:bg-gray-100 hover:text-sky-500"
+            @click="
+              ;[
+                emits('NameBoard', board.board.name),
+                router.replace({
+                  name: 'Task',
+                  params: { id: board.id.boardId }
+                })
+              ]
+            "
+          >
+            <div class="flex justify-between items-center">
+              <span class="font-bold text-lg">No : {{ index + 1 }}</span>
+            </div>
+            <div class="itbkk-board-name text-xl font-semibold">
+              Name :
+              {{
+                board.board?.name == undefined ? board.name : board.board.name
+              }}
+            </div>
+            <div class="itbkk-owner-name text-sm text-gray-500">
+              <p>
+                Owner : {{ board.role == undefined ? 'owner' : board.role }}
+              </p>
+            </div>
+            <div class="itbkk-access-right text-sm text-gray-500">
+              <p>
+                Access Right :
+                {{ board.role == undefined ? 'owner' : board.role }}
+              </p>
+            </div>
+            <div class="itbkk-leave-board text-sm text-gray-500">
+              <p>
+                Action :
+                <button
+                  class="itbkk-button-create bg-gray-300 text-sm rounded-[6px] font-sans text-gray-700 hover:text-white px-7 py-2 mr-2 my-3"
+                  @click=""
+                >
+                  Leave
+                </button>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
+    <teleport to="body" v-if="showAddNewBoard">
+      <AddNewBoard
+        @cancelDetail="clearDeletePopUp"
+        @saveDetail="showDelComplete"
+        @errorOccurred="showErrorMessage"
+      ></AddNewBoard>
+    </teleport>
   </div>
-  <div class="flex justify-center font-bold">
-    <h1 class="font-bold text-center cursor-default text-3xl py-3">
-      Collab Boards
-    </h1>
-  </div>
-  <div
-    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-  >
-    <!-- Grid layout for dashboard-like appearance -->
-    <div
-      v-for="(board, index) in boardsList"
-      :key="board.id.boardId"
-      class="itbkk-personal-item bg-white border rounded-lg shadow-md p-4 flex flex-col space-y-2 cursor-pointer hover:bg-gray-100 hover:text-sky-500"
-      @click="
-        ;[
-          emits('NameBoard', board.board.name),
-          router.replace({ name: 'Task', params: { id: board.id.boardId } })
-        ]
-      "
-    >
-      <div class="flex justify-between items-center">
-        <span class="font-bold text-lg">No : {{ index + 1 }}</span>
-        <!-- <div class="flex space-x-2">
-          <span>⚙️</span>
-          <span>🗑️</span>
-        </div> -->
-      </div>
-      <div class="itbkk-board-name text-xl font-semibold">
-        Name :
-        {{ board.board?.name == undefined ? board.name : board.board.name }}
-      </div>
-      <div class="text-sm text-gray-500">
-        <p>Action : {{ board.role == undefined ? 'owner' : board.role }}</p>
-      </div>
-      <div class="itbkk-board-visibility text-sm text-gray-500">
-        <p>Visibility : {{ board.role == undefined ? 'owner' : board.role }}</p>
-      </div>
-    </div>
-  </div>
-  <teleport to="body" v-if="showAddNewBoard">
-    <AddNewBoard
-      @cancelDetail="clearDeletePopUp"
-      @saveDetail="showDelComplete"
-      @errorOccurred="showErrorMessage"
-    ></AddNewBoard>
-  </teleport>
 </template>
