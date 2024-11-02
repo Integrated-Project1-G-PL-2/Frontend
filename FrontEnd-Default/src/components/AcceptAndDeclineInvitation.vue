dd2bb6effd6e521ba39376db23561062d6e7aaeb
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2024-11-03 00:34:05 +0700

    add

---
+++ FrontEnd-Default/src/components/AcceptAndDeclineInvitation.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const deClareemit = defineEmits(['cancelInvitationPopUp'])
+
+const props = defineProps(['isRemove', 'isAccept', 'operate'])
+const confirmAcceptInvatationCollab = async function () {}
+const confirmRemoveInvatationCollab = async function () {}
+</script>
+
+<template>
+  <div
+    class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[10px]"
+    v-if="props.isAccept"
+  >
+    <div
+      class="itbkk-modal-alert w-[30%] m-[auto] mt-[20%] border border-gray-600 h-[200px]"
+    >
+      <div class="flex flex-col justify-between bg-white p-4 h-[100px]">
+        <div class="itbkk-title w-full h-[70px] mt-1 border-b">
+          <h1 class="text-xl font-bold text-justify">Leave Board</h1>
+        </div>
+
+        <div class="w-[70%] h-[100%]">
+          <div class="itbkk-message pl-4 mt-4">
+            Do you want to accept this "{{
+              props.NameLeaveCollabBoard.value.name
+            }}" to become collaborator of the board?
+          </div>
+        </div>
+        <div class="flex flex-row w-full justify-end border-t h-[60%] mt-6">
+          <button
+            class="itbkk-button-confirm bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[60px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+            @click="confirmAcceptInvatationCollab()"
+          >
+            <div class="btn text-center">Confirm</div>
+          </button>
+
+          <button
+            class="itbkk-button-cancel bg-red-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+            @click=";[$emit('cancelInvitationPopUp', true)]"
+          >
+            <div class="btn text-center">Cancel</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="bg-grey-500 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 pt-[10px]"
+    v-if="props.isRemove"
+  >
+    <div
+      class="itbkk-modal-alert w-[30%] m-[auto] mt-[20%] border border-gray-600 h-[200px]"
+    >
+      <div class="flex flex-col justify-between bg-white p-4 h-[100px]">
+        <div class="itbkk-title w-full h-[70px] mt-1 border-b">
+          <h1 class="text-xl font-bold text-justify">Change Access Right</h1>
+        </div>
+
+        <div class="w-[70%] h-[100%]">
+          <div class="itbkk-message pl-4 mt-4">
+            Do you want this "
+            {{ props.NameChangeCollabBoard.value.name }}" to be remove from
+            board's collaborator
+          </div>
+        </div>
+        <div class="flex flex-row w-full justify-end border-t h-[60%] mt-6">
+          <button
+            class="itbkk-button-confirm bg-green-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[60px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+            @click="confirmRemoveInvatationCollab()"
+          >
+            <div class="btn text-center">Confirm</div>
+          </button>
+
+          <button
+            class="itbkk-button-cancel bg-red-400 scr-m:btn-sm scr-l:btn-md scr-l:rounded-[10px] rounded-[2px] w-[50px] h-[25px] font-sans btn-xs scr-l:btn-m text-center flex flex-col gap-2 hover:text-gray-200 mr-3 mt-4"
+            @click=";[$emit('cancelInvitationPopUp', true)]"
+          >
+            <div class="btn text-center">Cancel</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
