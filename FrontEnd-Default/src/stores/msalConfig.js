5af559980870abaa65f0dab97d124dcc0d5e3223
NoireChama <111353702+NoireChama@users.noreply.github.com>
2024-11-29 23:57:56 +0700

    microsoft login

---
+++ FrontEnd-Default/src/stores/msalConfig.js
@@ -0,0 +1,29 @@
+import { PublicClientApplication } from "@azure/msal-browser";
+import { reactive } from "vue";
+
+export const msalConfig = {
+  auth: {
+    clientId: "d129054b-4326-426a-a979-999c98603cc6",
+    authority:
+      "https://login.microsoftonline.com/79845616-9df0-43e0-8842-e300feb2642a",
+    redirectUri: "http://localhost:5173/callback/login",
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false,
+  },
+  system: {
+    responseMode: "query",
+  },
+};
+
+export const graphScopes = {
+  scopes: ["user.read", "openid", "profile", "offline_access", "email"],
+};
+
+export const state = reactive({
+  isAuthenticated: false,
+  user: null,
+});
+
+export const msalInstance = new PublicClientApplication(msalConfig);
